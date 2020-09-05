1f76071d2cabf37d052eb3a4728c098cdc7cf163
Juan Ignacio Ruiz <juanignacioruiz540@gmail.com>
2020-09-05 11:25:14 -0300

    intento

---
+++ intento/intento/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("intento");
+        }
+    }
+}
